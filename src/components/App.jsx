7f8ba80b5a53c3cd594e0e224d5a00ae64dbbb49
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-05-22 21:01:53 +0300

    fixed bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,9 @@ import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader/Loader';
 import Button from './Button/Button';
 
-import { fetchImg } from './Servises/api';
+import { URL, API_KEY, fetchImg } from './Servises/api';
 
 const App = () => {
-  const URL = 'https://pixabay.com/api/';
-  const API_KEY = '34966777-a1579da70d4a26e0e4c8e2fcd';
-
   const [pictures, setPictures] = useState([]);
   const [error, setError] = useState('');
   const [status, setStatus] = useState('idle');
@@ -22,29 +19,29 @@ const App = () => {
   const [showBtn, setShowBtn] = useState(false);
 
   useEffect(() => {
-    if (query !== '' || page !== 1) {
-      setStatus('pending');
-      fetchImg(URL, API_KEY, query, page)
-        .then((pictures) => {
-          if (!pictures.total) {
-            toast.error('Did not find anything, mate');
-            return;
-          }
-          const selectedProperties = pictures.hits.map(({ id, largeImageURL, webformatURL }) => ({
-            id,
-            largeImageURL,
-            webformatURL,
-          }));
-          setPictures((prevPictures) => [...prevPictures, ...selectedProperties]);
-          setStatus('resolved');
-          setShowBtn(page < Math.ceil(pictures.totalHits / 12));
-        })
-        .catch((error) => {
-          setError(error.message);
-          setStatus('rejected');
-        });
-    }
-  }, [URL, API_KEY, query, page]);
+    if (!query) return;
+
+    setStatus('pending');
+    fetchImg(URL, API_KEY, query, page)
+      .then((pictures) => {
+        if (!pictures.total) {
+          toast.error('Did not find anything, mate');
+          return;
+        }
+        const selectedProperties = pictures.hits.map(({ id, largeImageURL, webformatURL }) => ({
+          id,
+          largeImageURL,
+          webformatURL,
+        }));
+        setPictures((prevPictures) => [...prevPictures, ...selectedProperties]);
+        setStatus('resolved');
+        setShowBtn(page < Math.ceil(pictures.totalHits / 12));
+      })
+      .catch((error) => {
+        setError(error.message);
+        setStatus('rejected');
+      });
+  }, [query, page]);
 
   const processSubmit = (query) => {
     setQuery(query);
@@ -70,62 +67,3 @@ const App = () => {
 };
 
 export default App;
-
-// const App = () => {
-//   const [URL, setURL] = useState('https://pixabay.com/api/');
-//   const [API_KEY, setAPI_KEY] = useState('34966777-a1579da70d4a26e0e4c8e2fcd');
-//   const [pictures, setPictures] = useState([]);
-//   const [error, setError] = useState('');
-//   const [status, setStatus] = useState('idle');
-//   const [page, setPage] = useState(1);
-//   const [query, setQuery] = useState('');
-//   const [showBtn, setShowBtn] = useState(false);
-
-//   useEffect(() => {
-//     if (query !== '' || page !== 1) {
-//       setStatus('pending');
-//       fetchImg(URL, API_KEY, query, page)
-//         .then(pictures => {
-//           if (!pictures.total) {
-//             toast.error('Did not find anything, mate');
-//             return;
-//           }
-//           const selectedProperties = pictures.hits.map(({ id, largeImageURL, webformatURL }) => ({
-//             id,
-//             largeImageURL,
-//             webformatURL,
-//           }));
-//           setPictures(prevPictures => [...prevPictures, ...selectedProperties]);
-//           setStatus('resolved');
-//           setShowBtn(page < Math.ceil(pictures.totalHits / 12));
-//         })
-//         .catch(error => {
-//           setError(error);
-//           setStatus('rejected');
-//         });
-//     }
-//   }, [URL, API_KEY, query, page]);
-
-//   const processSubmit = query => {
-//     setQuery(query);
-//     setPictures([]);
-//     setPage(1);
-//     setShowBtn(true);
-//   };
-
-//   const handleLoadMore = () => {
-//     setPage(prevPage => prevPage + 1);
-//   };
-
-//   return (
-//     <>
-//       <Searchbar onSubmit={processSubmit} />
-//       {pictures.length > 0 && <ImageGallery images={pictures} />}
-//       {showBtn && <Button onClick={handleLoadMore} />}
-//       {status === 'pending' && <Loader />}
-//       <ToastContainer autoClose={2000} />
-//     </>
-//   );
-// };
-
-// export default App;
--- src/components/Servises/api.js
+++ src/components/Servises/api.js
@@ -1,3 +1,6 @@
+export const URL = 'https://pixabay.com/api/';
+export const API_KEY = '34966777-a1579da70d4a26e0e4c8e2fcd';
+
 export const fetchImg = (URL, API_KEY, query, page) => {
   return fetch(
     `${URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
